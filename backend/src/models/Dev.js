ed798da989e676bf9fed7fe2d626995b25ca866f
Lucas Dias <lucaspdpp@gmail.com>
2019-08-09 13:18:48 -0300

    Termino da Aula

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -13,7 +13,12 @@ const DevSchema = new Schema({
     avatar: {
         type: String,
         required: true
-    }
+    },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev'
+    }],
+    dislikes: []
 }, {
     timestamps: true
 });
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const server = express();
 
-server.use(express.json());
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0-jyby7.mongodb.net/omnistack?retryWrites=true&w=majority',{
     useNewUrlParser: true
 });
+
+server.use(cors());
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
